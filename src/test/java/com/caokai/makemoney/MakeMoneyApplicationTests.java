7fc504790811664b60f1e125272092172154d81c
caokai <caoxiaokai1994@gmail.com>
2024-07-12 16:59:32 +0800

    project init

---
+++ src/test/java/com/caokai/makemoney/MakeMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caokai.makemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
